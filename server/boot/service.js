f21ebd1dfce5f0d1b7330600b708c298ab1d6e5a
monthiraa <monthira@playwork.co.th>
2018-04-09 17:03:47 +0700

    update script create address

--- server/boot/service.js
+++ server/boot/service.js
@@ -1,4 +1,5 @@
 const exec = require('child_process').exec;
+const Fs = require('fs');
 module.exports = function(app) {
   var User = app.models.user;
   var Role = app.models.Role;
@@ -64,15 +65,38 @@ module.exports = function(app) {
   });
 
   Province.find({}, function(err, province) {
-    // console.log(province.length);
-    if (province.length === 0) {
-       exec('sh addressInformation/import.sh', (error, stdout, stderr) => {
+    var obj;
+    var db;
+    Fs.readFile(__dirname + '/../datasources.json', 'utf8', function(err, data) {
+      if (err) throw err;
+      obj = JSON.parse(data);
+      db = obj.mongoDb.database;
+      let provinceCollection = `mongoimport --db ${db} --collection Province < ./addressInformation/province.json`
+      let districtCollection = `mongoimport --db ${db} --collection District < ./addressInformation/district.json`
+      let amphurCollection = `mongoimport --db ${db} --collection Amphur < ./addressInformation/amphur.json`
+      if (province.length === 0) {
+        exec(provinceCollection, (error, stdout, stderr) => {
           if (error !== null) {
             console.log(`exec error: ${error}`);
           }
-          // console.log(`${stdout}`);
-          // console.log(`${stderr}`);
+          exec(districtCollection, (error, stdout, stderr) => {
+            if (error !== null) {
+              console.log(`exec error: ${error}`);
+            }
+            exec(amphurCollection, (error, stdout, stderr) => {
+              if (error !== null) {
+                console.log(`exec error: ${error}`);
+              }
+              console.log('amphurCollection Success',`${stdout}`);
+              console.log('amphurCollection Err',`${stderr}`);
+            });
+            console.log('districtCollection Success',`${stdout}`);
+            console.log('districtCollection Err',`${stderr}`);
+          });
+          console.log('provinceCollection Success',`${stdout}`);
+          console.log('provinceCollection Err',`${stderr}`);
         });
-    }
+      };
+    });
   });
 };
